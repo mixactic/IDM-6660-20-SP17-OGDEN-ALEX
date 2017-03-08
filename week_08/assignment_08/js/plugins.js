4522169925155cd97873fc2a5b004741caa26e09
Forever Fox <cogden@mcad.edu>
2017-03-08 02:41:47 +0200

    Week 8 Assignment

---
+++ week_08/assignment_08/js/plugins.js
@@ -0,0 +1,32 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+
+$.preloadImages = function() {
+  for (var i = 0; i < arguments.length; i++) {
+    $("<img />").attr("src", "../img/"+arguments[i]);
+  }
+}
+
+$.preloadImages("background-1.jpg","background-2.jpg","wall-1.jpg","wall-2.jpg","wall-3.jpg");
